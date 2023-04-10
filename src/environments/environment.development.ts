c0c30f603faed875504ce5c3c762c4d953bb190a
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-04-11 00:06:52 +0300

    add env files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyD7uCLd-Drkj65BXty2RZ-4pXlXldR1POs",
+    authDomain: "heroes-app-26908.firebaseapp.com",
+    projectId: "heroes-app-26908",
+    storageBucket: "heroes-app-26908.appspot.com",
+    appId: "1:867818934620:web:8c72ac2ac48922e7163199"
+  }
+};
