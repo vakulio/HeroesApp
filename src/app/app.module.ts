121ca7e9f90b47ec30569c77d045e026cefb627d
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-04-10 18:50:21 +0300

    create based components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    NotFoundComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
