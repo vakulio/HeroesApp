e550eb5caa14586d2300607f7161a5aa67852133
Vakula Vladimir <42750233+vakulio@users.noreply.github.com>
2023-05-10 23:30:03 +0300

    add account table

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,34 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { IUserDB } from '../models/user.models';
 import { BattleService } from '../services/battle.service';
 import { IBattle } from '../models/battle.models';
-
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [DatePipe],
 })
 export class AccountComponent implements OnInit {
   userData: IUserDB | null = null;
   userBattles: IBattle[] = [];
+  displayedColumns: string[] = [
+    'userName',
+    'timestamp',
+    'heroName',
+    'enemyName',
+    'result',
+  ];
 
-  constructor(public user: AuthService, public battle: BattleService) {}
+  constructor(
+    public user: AuthService,
+    public battle: BattleService,
+    private cd: ChangeDetectorRef
+  ) {}
   ngOnInit(): void {
     this.user.getUserData().subscribe((docs) => {
-      docs.forEach((doc) => {
-        this.userData = doc.data();
+      const userData = docs.map((doc) => {
+        return {
+          ...doc.data(),
+        };
       });
+      this.userData = userData[0];
     });
     this.battle.getUserBattles().subscribe((docs) => {
-      this.userBattles = [];
-      docs.forEach((doc) => {
-        this.userBattles.push({
-          docID: doc.id,
+      const battleData = docs.map((doc) => {
+        return {
           ...doc.data(),
-        });
+        };
       });
+      this.userBattles = battleData;
+      this.cd.detectChanges();
     });
   }
 }
